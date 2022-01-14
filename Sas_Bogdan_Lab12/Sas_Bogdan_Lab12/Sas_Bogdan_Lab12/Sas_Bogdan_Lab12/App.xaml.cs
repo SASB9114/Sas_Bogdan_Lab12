9066eb0a2eaf10bce0671273d4d7006a4dba6aae
SASB9114 <92628191+SASB9114@users.noreply.github.com>
2022-01-14 19:25:27 +0200

    Lab12
    Lab12

---
+++ Sas_Bogdan_Lab12/Sas_Bogdan_Lab12/Sas_Bogdan_Lab12/Sas_Bogdan_Lab12/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Sas_Bogdan_Lab12.Data;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sas_Bogdan_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Sas_Bogdan_Lab12/Sas_Bogdan_Lab12/Sas_Bogdan_Lab12/Sas_Bogdan_Lab12/Data/IRestService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Sas_Bogdan_Lab12.Models;
+
+namespace Sas_Bogdan_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
